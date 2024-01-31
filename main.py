b8d6728033d9bdaf8891ebcbd4083736e5b0654d
rafzarf <xxrafzaxx@gmail.com>
2024-01-31 14:53:27 +0700

    Mengubah cmap

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ xi, yi = np.meshgrid(np.linspace(0, 7, 100), np.linspace(0, 7, 100))
 zi = griddata((x.flatten(), y.flatten()), sensor_data.flatten(), (xi, yi), method='cubic')
 
 # Create the heatmap
-plt.imshow(zi, extent=(0, 7, 0, 7), origin='lower', cmap='viridis')
+plt.imshow(zi, extent=(0, 7, 0, 7), origin='lower', cmap='plasma')
 
 # Add colorbar for reference
 plt.colorbar()
